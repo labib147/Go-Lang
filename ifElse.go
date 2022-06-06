de85f4f712b86b58a2b03516feed4448510917b9
labib147 <nefaurrahmanlabib147@gmail.com>
2022-06-06 13:36:45 +0600

    com

--- ifElse.go
+++ ifElse.go
@@ -7,8 +7,8 @@ import (
 func main() {
 	x := 2
 
-	if x > 6 {
-		fmt.Println("More than 6")
+	if x >= 6 {
+		fmt.Println("More than 6 or equal 6 ")
 	} else if x <= 2 {
 		fmt.Println("Less than or equal 2")
 	} else {
