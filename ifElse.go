68f7b731d39310a9573da70cc8989f0e0a0cc2bc
labib147 <nefaurrahmanlabib147@gmail.com>
2022-06-06 13:27:25 +0600

    com

---
+++ ifElse.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := 2
+
+	if x > 6 {
+		fmt.Println("More than 6")
+	} else if x <= 2 {
+		fmt.Println("Less than or equal 2")
+	} else {
+		fmt.Println("Between 2 and 6")
+	}
+}
