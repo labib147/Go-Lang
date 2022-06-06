c7b6c47b78750a702ee4c88f6a5791cc8b64e25e
labib147 <nefaurrahmanlabib147@gmail.com>
2022-06-06 12:44:32 +0600

    commit

--- varTyped.go
+++ varTyped.go
@@ -5,6 +5,12 @@ import (
 )
 
 func main() {
+	/*
+		:= can be used for both declaration and assignment
+		= is only used for assignment
+	*/
+	//var x int = 5 & x:= 5 are the same things
+
 	x := 7
 	y := 8
 	sum := x + y
