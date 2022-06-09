bde0f7d1574fbb4aaf447f8832593520f2d19cd8
labib147 <nefaurrahmanlabib147@gmail.com>
2022-06-10 00:19:01 +0600

    15

--- 9_15.pointer.go
+++ 9_15.pointer.go
@@ -6,12 +6,23 @@ import (
 
 func main() {
 	i := 7
+	inc(&i) // we have to use & to send the pointer
 	fmt.Println(i)
 	fmt.Println(&i) //& - to get the memory address of the variable
 }
 
+func inc(x *int) { // Prefixing the type with *
+	*x++ // Reference the pointer to increment from. Without this it will be incrementing the memory address.
+}
+
 /*
-func inc(x int) {	// The increment function gets a copy of i
+func main() {
+	i := 7
+	inc(i)
+	fmt.Println(i)
+}
+
+func inc(x int) {	// i is copied by value so the increment function gets a copy of i
 	x++	// Does not save the value because were not returning anything
 }
 */
